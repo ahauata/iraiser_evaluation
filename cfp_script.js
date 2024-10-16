df416d6c2b5ff14e75af41cdbf5dc1079282cbd6
TomPast <pastor.tom.etu@gmail.com>
2024-10-16 22:09:50 +0200

    feat: remove TOS link from footer

--- cfp_script.js
+++ cfp_script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const footerLinks = document.querySelectorAll(
+    "footer a[href*='conditions-generales']"
+  );
 
+  footerLinks.forEach((link) => {
+    const parentLi = link.closest("li");
+    if (parentLi) {
+      parentLi.remove();
+    }
+  });
+});
