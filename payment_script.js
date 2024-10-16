979f12f0ea26ed772e16ac05fc229951cd580525
TomPast <pastor.tom.etu@gmail.com>
2024-10-16 23:28:16 +0200

    feat: change credit card label to visa

--- payment_script.js
+++ payment_script.js
@@ -1,1 +1,26 @@
+function replaceCreditCardText() {
+  const cardElements = document.querySelectorAll(
+    ".payment_mode-card[data-paymentid='payment_mode-card']"
+  );
 
+  cardElements.forEach((element) => {
+    const creditCardTextElement = element.querySelector(".payment_mode_title");
+    if (creditCardTextElement) {
+      creditCardTextElement.textContent = "VISA";
+    }
+  });
+}
+
+function initPaymentScript() {
+  replaceCreditCardText();
+
+  if (typeof Step3 !== "undefined" && Step3.showPaymentModes) {
+    const originalShowPaymentModes = Step3.showPaymentModes;
+    Step3.showPaymentModes = function (frequency) {
+      originalShowPaymentModes.call(Step3, frequency);
+      replaceCreditCardText();
+    };
+  }
+}
+
+document.addEventListener("DOMContentLoaded", initPaymentScript);
